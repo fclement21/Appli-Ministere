9822d4169918edb512f4835d31aaad147fc3732d
dANO <florianclement21@gmail.com>
2013-05-23 11:20:35 +0200

    Scaffold de Section

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Redmine::Application.routes.draw do
+  resources :sections
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +51,7 @@ Redmine::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'sections#index'
 
   # See how all your routes lay out with "rake routes"
 
