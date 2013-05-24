acfd98916a79e9da1957aeae8de9d8ebd3c6337e
dANO <florianclement21@gmail.com>
2013-05-24 16:12:36 +0200

    Bug sur la boucle article , nil:Class en train de la fixer.

--- config/routes.rb
+++ config/routes.rb
@@ -57,7 +57,7 @@ Redmine::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'sections#index'
+
 
   # See how all your routes lay out with "rake routes"
 
