737c17cedfe2b374d4726c53d9ab3c834b3f2d93
dANO <florianclement21@gmail.com>
2013-05-28 15:56:48 +0200

    Redirection de Section fini. Redimensionnement du markitup

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,4 +1,23 @@
 class SectionsController < InheritedResources::Base
+actions :show, :new, :create, :edit, :update
+
+def destroy
+  super do |format|
+    format.html { redirect_to :controller =>'dashboard', :action =>'index'}
+  end
+end
+
+def create
+  super do |format|
+    format.html { redirect_to :controller =>'dashboard', :action =>'index'}
+  end
+end
+
+def update
+  super do |format|
+    format.html { redirect_to :controller =>'dashboard', :action => 'index'}
+  end
+end
 
 
 
