4f1a283e26f8b8ccebf5157b27a63aa2165fdcf2
dANO <florianclement21@gmail.com>
2013-05-30 11:26:55 +0200

    Modification du menu. Deplacement du css + Redirect des controllers.

--- app/controllers/divisions_controller.rb
+++ app/controllers/divisions_controller.rb
@@ -1,2 +1,47 @@
 class DivisionsController < InheritedResources::Base
+  def destroy
+  super do |format|
+    format.html { redirect_to :controller =>'dashboard', :action =>'index'}
+  end
+   if @division.destroy
+
+      else
+        flash[:avertissement] = "Erreur lors de la suppression"
+    end
+end
+
+def create
+
+   if @division.save
+redirect_to :controller =>'dashboard', :action =>'index'
+      else
+        flash[:avertissement] = "Veuillez remplir le champs titre."
+        redirect_to new_division_path
+    end
+end
+
+def update
+  super do |format|
+    format.html { redirect_to :controller =>'dashboard', :action => 'index'}
+if @division.save
+
+else
+    flash[:avertissement] = "Titre obligatoire"
+  end
+  end
+end
+
+def new
+ if Division.save
+redirect_to :controller =>'dashboard', :action =>'index'
+else
+  flash[:avertissement] = "Veuillez remplir le champs titre."
+
+end
+ end
+
+
+
+
+
 end
