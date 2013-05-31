4e2a4876f8c0c34f68a37b01bc04ad03a53de107
dANO <florianclement21@gmail.com>
2013-05-31 15:01:45 +0200

    Dashboard user done, fix bug sur les boutons.

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Redmine::Application.routes.draw do
   get "users/index"
 
 root to: "sections#show", :id => '29', :division_id => '5'
+resources :dashboard
  resources :divisions do
   resources :sections do
     resources :articles
     end
   end
-resources :dashboard
+
 devise_for :users, :path_names => { :sign_in => "login"}
 
 
