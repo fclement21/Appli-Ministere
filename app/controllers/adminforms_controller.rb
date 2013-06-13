2610bfe6b104e596bd6afaa8037a22419937eac2
fclement21 <florianclement21@gmail.com>
2013-06-13 11:22:09 +0200

    Modification du fichier Readme et du css

--- app/controllers/adminforms_controller.rb
+++ app/controllers/adminforms_controller.rb
@@ -1,6 +1,22 @@
 class AdminformsController < InheritedResources::Base
  before_filter :admin_user
 
+def update
+
+    @adminform = Adminform.find(params[:id])
+
+   respond_to do |format|
+     if @adminform.update_attributes(params[:adminform])
+        format.html { redirect_to edit_adminform_path(1), notice: 'Formulaire change.' }
+
+
+      else
+        flash[:avertissement] = "Tout les champs sont obligatoires"
+        format.html { render action: "edit" }
+        format.json { render json: @adminform.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
  private
 def admin_user
